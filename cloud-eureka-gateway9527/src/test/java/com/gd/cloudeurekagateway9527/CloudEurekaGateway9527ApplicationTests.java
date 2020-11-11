814a361bbc0513e9a508cf0ce203fe0d414b3d9a
zhaojun <6982188@qq.com>
2020-11-11 18:12:31 +0800

    add files

---
+++ cloud-eureka-gateway9527/src/test/java/com/gd/cloudeurekagateway9527/CloudEurekaGateway9527ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.cloudeurekagateway9527;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaGateway9527ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
